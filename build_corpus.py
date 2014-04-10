f68748f5348b44511f4c50e2e482e46a6809dd35
Stephen Lee <stephen.lee@10gen.com>
2014-04-10 15:23:39 -0400

    More mucking through raw data

--- build_corpus.py
+++ build_corpus.py
@@ -3,18 +3,22 @@ import argparse
 import re
 import nltk
 
+from collections import defaultdict
+
 from nltk.corpus import stopwords
 
 class CorpusBuilder(object):
-    def __init__(self, hp, db, coll, query=None):
+    def __init__(self, hp, db, coll, lb=0, query=None):
         self.conn = pymongo.MongoClient(hp)[db][coll]
         self.query = query   # TODO
         self.wl = nltk.WordNetLemmatizer()
-        self.vocabulary = set()
+        self.vocabulary = defaultdict(int)
         self.stop = set(stopwords.words('english'))
+        self.lb = lb
 
     def build(self):
-        for d in self.conn.find({}, ['fields.description', 'fields.comment.comments']):
+        for d in self.conn.find({'fields.issuetype.name': {'$ne': "Tracking"}},
+                                ['key', 'fields.description', 'fields.comment.comments']):
             try:
                 doc = "%s\n\n%s" % (d['fields']['description'],
                                     '\n\n'.join([c['body'] for c in d['fields']['comment']['comments']]))
@@ -30,7 +34,7 @@ class CorpusBuilder(object):
                 clean = re.sub('\[.*?\]', ' ', clean)
 
                 clean = re.sub('\s', ' ', clean)
-                clean = re.sub(r'\b([A-Za-z]+[!#-_\./:]+)+[A-Za-z]+\b', ' ', clean)
+                clean = re.sub(r'\b([A-Za-z]*[-:/\._0-9]+)+[A-Za-z]*\b', ' ', clean)
                 clean = re.sub('[^A-Za-z ]', '', clean)
                 clean = re.sub('\s+', ' ', clean)
                 doc = []
@@ -38,14 +42,14 @@ class CorpusBuilder(object):
                 for w in clean.split():
                     word = self.wl.lemmatize(w.lower())
                     doc.append(word)
-                    self.vocabulary.add(word)
+                    self.vocabulary[word] += 1
 
-                yield doc
+                yield {'key': d['key'], 'doc': doc}
             except:
                 pass
 
     def dictionary(self):
-        return self.vocabulary.difference(self.stop)
+        return [p[0] for p in self.vocabulary.items() if p[0] not in self.stop and p[1] > self.lb and len(p[0]) > 2]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -55,17 +59,17 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--collection', metavar='COLLECTION', default='issues')
     parser.add_argument('-q', '--query', metavar='QUERY')
     parser.add_argument('-f', '--file', metavar='FILE')
+    parser.add_argument('-l', '--lowerbound', type=int, default=10, metavar='FILE')
     parser.add_argument('-v', '--vocabulary', metavar='FILE')
     args = parser.parse_args()
 
-    cb = CorpusBuilder(args.server, args.database, args.collection)
+    cb = CorpusBuilder(args.server, args.database, args.collection, args.lowerbound)
     f = open(args.file, 'w')
 
     for i, t in enumerate(cb.build()):
         try:
-            f.write("%s\n" % ' '.join(t))
-            f.flush()
-            print 'Done with doc %d' % (i + 1)
+            f.write("%s\n" % ' '.join(t['doc']))
+            print 'Done with doc %d: %s' % (i + 1, t['key'])
         except:
             pass
 
@@ -75,6 +79,5 @@ if __name__ == '__main__':
 
     for w in cb.dictionary():
         f.write('%s\n' % w)
-        f.flush()
 
     f.close()
