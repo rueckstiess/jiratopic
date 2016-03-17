a9daa41603c7f6513f23c84a1aa35e9b0e59cd04
Thomas Rueckstiess <thomas@rueckstiess.net>
2016-03-17 19:00:41 +1100

    added all the stuff that's not in master.

--- backend.py
+++ backend.py
@@ -7,7 +7,7 @@ import json
 from datetime import datetime, timedelta
 
 
-def group_by_weeks(coll, start_date, end_date):
+def group_by_weeks(coll, start_date, end_date, normalize=True):
     # compute first Monday >= start_date
     if start_date.weekday() > 0:
         start_date += timedelta(days=(7 - start_date.weekday()))
@@ -25,7 +25,11 @@ def group_by_weeks(coll, start_date, end_date):
         try:
             mat = n.matrix([d['topic_scores'] for d in coll.find(query, ['topic_scores'])])
             rowsum = mat.sum(axis=1)
+            #norm = mat / rowsum.reshape(rowsum.shape[0], 1) / mat.shape[0]
             norm = mat / rowsum.reshape(rowsum.shape[0], 1)
+
+            if normalize:
+                norm /= mat.shape[0]
 
             yield {
                 'start_date': i.isoformat(),
@@ -47,8 +51,9 @@ class TopicsScoresHandler(tornado.web.RequestHandler):
         start_date = datetime.strptime(self.get_argument('s'), '%Y%m%d')
         end_date = datetime.strptime(self.get_argument('e'), '%Y%m%d')
         end_date += timedelta(days=1) - timedelta(microseconds=1)
+        normalize = self.get_argument('n', "true")
         self.set_header('Content-type', 'application/json')
-        self.write(json.dumps([w for w in group_by_weeks(self.coll, start_date, end_date)]))
+        self.write(json.dumps([w for w in group_by_weeks(self.coll, start_date, end_date, normalize == "true")]))
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -59,7 +64,7 @@ if __name__ == "__main__":
     application = tornado.web.Application([
         (r'/', MainHandler),
         (r"/topics", TopicsScoresHandler, {
-            'connection': pymongo.MongoClient('localhost:12345'),
+            'connection': pymongo.MongoClient('localhost:27017'),
             'database': 'jira',
             'collection': 'issues'})],
         debug=True, static_path="static",
